03b634fc8ed06c4aa1c152dbe0db6e52cd9ef5ed
Alan <yashikalana@mail.ru>
2025-08-16 13:36:45 +0300

    Initial Commit

---
+++ GitSourseControl/GitSourseControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourseControlApp.swift
+//  GitSourseControl
+//
+//  Created by Алан Парастаев on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct GitSourseControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
