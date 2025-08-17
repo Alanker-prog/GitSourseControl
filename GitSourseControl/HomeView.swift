38272e941b8721b8cb758986c709db15ee54cec0
Alan <yashikalana@mail.ru>
2025-08-17 10:48:04 +0300

    изменил text на Screen two

---
+++ GitSourseControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitSourseControl
+//
+//  Created by Алан Парастаев on 17.08.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen two")
+    }
+}
+
+#Preview {
+    HomeView()
+}
