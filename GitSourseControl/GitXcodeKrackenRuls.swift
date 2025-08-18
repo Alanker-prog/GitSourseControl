7520dce145b6f52d6be125a3ccf9b6aeef68f81b
Alan <yashikalana@mail.ru>
2025-08-18 14:49:04 +0300

    [дока] обновил доку по Git

---
+++ GitSourseControl/GitXcodeKrackenRuls.swift
@@ -0,0 +1,21 @@
+//
+//  GitXcodeKrackenRuls.swift
+//  GitSourseControl
+//
+//  Created by Алан Парастаев on 18.08.2025.
+//
+
+import SwiftUI
+/*
+ Ветки ✅
+ Если хотим объеденить ветки, переключаемся на основную ветку(main) и далее правой кнопкой мыши жмем на дополнительную ветку и нажимаем Merge " доп ветка " into "main" 
+ */
+struct GitXcodeKrackenRuls: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitXcodeKrackenRuls()
+}
