da151cb0087013795400728ec4b7accf21282f3b
Alan <yashikalana@mail.ru>
2025-08-21 19:02:59 +0300

    [doka] добавил доку в ContentView

---
+++ GitSourseControl/10.PullRequests.swift
@@ -0,0 +1,18 @@
+//
+//  10.PullRequests.swift
+//  GitSourseControl
+//
+//  Created by Алан Парастаев on 21.08.2025.
+//
+
+import SwiftUI
+
+struct _0_PullRequests: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _0_PullRequests()
+}
