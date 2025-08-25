42902d19c5ca57830317a90b5332f6e3886bd7d6
Alan <yashikalana@mail.ru>
2025-08-25 11:35:23 +0300

    [doka] 12 Урок не закончиная версия

---
+++ GitSourseControl/12.GitIgnoreAndReadMe.swift
@@ -0,0 +1,29 @@
+//
+//  12.GitIgnoreAndReadMe.swift
+//  GitSourseControl
+//
+//  Created by Алан Парастаев on 24.08.2025.
+//
+
+import SwiftUI
+/*
+ ✴️ .gitignore файл это котрый позволяет игнорить(не коммитить) не нужные файлы на Github
+ 
+ ❌ Если я когда-то захочу закоммитить файл в проекте , а он не комитится возможно нужно будет зайти в папку .gitignore и удалить этот файл из списка игнорируемых
+ ❌ Добавляем файл .gitignore в каждый новый проект!!!
+ ✅ Как добавить .gitignore файл на Github(это делается в Github) -> просим чат GPT создать для нас этот файл(Создай пожалуйста .gitignore файл для Xcode и Swift проектов, или скопируй его в этом репозитории GitSourseControl в папке .gitignore) -> заходим на репозиторий в который хотим добавить .gitignore файл -> жмем Add file -> Create new file -> задаем имя файлу ОНО ДОЛЖНО БЫТЬ СТРОГО ТОЛЬКО ТАКИМ ->.gitignore‼️ -> далее копируем файл и вставляем его в большое окно в низу -> жмем Commit changes... -> жмем Commit directly to the main branch -> жмем подтвердение
+ 
+ ❌ Добавить файл ReadMe на мою гдавную страницу Github , позже пока я не стал этого делать ->  СМОТРИ В УРОКЕ №12
+ ✴️ ФАЙЛ ReadMe Я НЕ БУДУ СЕЙЧАС ДЕЛАТЬ ,ПОЗЖЕ ПОПРОСИ СОЗДАТЬ ФАБЛОН ФАЙЛА чат GPT (Создай пожалуйста шаблон файла ReadMe для моего проекта на Github) , КУДА ВСТАВИТЬ ЭТОТ ФАЙЛ СМОТРИ В УРОКЕ №12
+ 
+ */
+
+struct GitIgnoreAndReadMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitIgnoreAndReadMe()
+}
